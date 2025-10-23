e3a36e4d65302d4e81ac4312a11103debad8a87e
jzi <jzi@sitecore.net>
2025-10-23 16:46:30 -0400

    Changed component structure

---
+++ industry-verticals/retail/src/components/search/SearchResultsComponent.tsx
@@ -0,0 +1,180 @@
+'use client';
+import { useState } from 'react';
+
+import { GridIcon, ListBulletIcon } from '@radix-ui/react-icons';
+import type { SearchResultsInitialState, SearchResultsStoreState } from '@sitecore-search/react';
+import { WidgetDataType, useSearchResults, widget } from '@sitecore-search/react';
+import React from 'react';
+import { HIGHLIGHTED_ARTICLES_RFKID } from '@/_data/customizations';
+import HomeHighlighted from './HomeHighlighted';
+import Spinner from './Spinner';
+import ArticleItemCard from './ArticleCard';
+import SortOrder from './SortOrder';
+import ArticleHorizontalItemCard from './ArticleHorizontalCard';
+import SearchPagination from './SearchPagination';
+import SearchFacets from './SearchFacets';
+import ResultsPerPage from './ResultsPerPage';
+import QueryResultsSummary from './QueryResultsSummary';
+import { Filter } from 'lucide-react';
+import CardViewSwitcher from './CardViewSwitcher';
+
+const SEARCH_CONFIG = {
+  source: process.env.NEXT_PUBLIC_SEARCH_SOURCE as string,
+};
+
+export type ArticleModel = {
+  id: string;
+  type?: string;
+  title?: string;
+  name?: string;
+  subtitle?: string;
+  url?: string;
+  description?: string;
+  content_text?: string;
+  image_url?: string;
+  source_id?: string;
+};
+type ArticleSearchResultsProps = {
+  defaultSortType?: SearchResultsStoreState['sortType'];
+  defaultPage?: SearchResultsStoreState['page'];
+  defaultItemsPerPage?: SearchResultsStoreState['itemsPerPage'];
+  defaultKeyphrase?: SearchResultsStoreState['keyphrase'];
+};
+type InitialState = SearchResultsInitialState<'itemsPerPage' | 'keyphrase' | 'page' | 'sortType'>;
+
+export const SearchResultsComponent = ({
+  defaultSortType = 'featured_desc',
+  defaultPage = 1,
+  defaultKeyphrase = '',
+  defaultItemsPerPage = 10,
+}: ArticleSearchResultsProps) => {
+  const {
+    actions: { onItemClick },
+    state: { sortType, page, itemsPerPage },
+    queryResult: {
+      isLoading,
+      isFetching,
+      data: {
+        total_item: totalItems = 0,
+        sort: { choices: sortChoices = [] } = {},
+        facet: facets = [],
+        content: articles = [],
+      } = {},
+    },
+  } = useSearchResults<ArticleModel, InitialState>({
+    state: {
+      sortType: defaultSortType,
+      page: defaultPage,
+      itemsPerPage: defaultItemsPerPage,
+      keyphrase: defaultKeyphrase,
+    },
+    query: (query): any => {
+      if (SEARCH_CONFIG.source !== '') {
+        const sources = SEARCH_CONFIG.source.split('|');
+        sources.forEach((source) => {
+          query.getRequest().addSource(source.trim());
+        });
+      }
+    },
+  });
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  const defaultCardView = 'list';
+  const [dir, setDir] = useState(defaultCardView);
+  const onToggle = (value = defaultCardView) => setDir(value);
+  console.log('DEFAULT ', defaultKeyphrase);
+
+  if (isLoading) {
+    return (
+      <div className="flex h-screen w-full items-center justify-center">
+        <Spinner loading />
+      </div>
+    );
+  }
+  return (
+    <div>
+      <div className="text-opacity-75 relative flex max-w-full px-4 text-black dark:text-gray-100">
+        {isFetching && (
+          <div className="fixed top-0 right-0 bottom-0 left-0 z-30 h-full w-full bg-white opacity-50 dark:bg-gray-800">
+            <div className="absolute top-[50%] left-[50%] z-40 flex -translate-x-[50%] -translate-y-[50%] flex-col items-center justify-center">
+              <Spinner loading />
+            </div>
+          </div>
+        )}
+        {totalItems > 0 && (
+          <React.Fragment key="1">
+            <section className="relative mt-4 mr-8 flex w-[25%] flex-none flex-col">
+              <Filter />
+
+              <SearchFacets facets={facets} />
+            </section>
+            <section className="flex flex-[4_1_0%] flex-col">
+              {/* Sort Select */}
+              <section className="flex justify-between text-xs">
+                {totalItems > 0 && (
+                  <QueryResultsSummary
+                    currentPage={page}
+                    itemsPerPage={itemsPerPage}
+                    totalItems={totalItems}
+                    totalItemsReturned={articles.length}
+                  />
+                )}
+                <div>
+                  <CardViewSwitcher
+                    onToggle={onToggle}
+                    defaultCardView={defaultCardView}
+                    GridIcon={GridIcon}
+                    ListIcon={ListBulletIcon}
+                  />
+                  <SortOrder options={sortChoices} selected={sortType} />
+                </div>
+              </section>
+
+              {/* Results */}
+              {dir === 'grid' ? (
+                <div className="grid grid-cols-2 gap-x-3 gap-y-3 md:grid-cols-3 md:gap-x-5 lg:grid-cols-4 xl:gap-x-6 xl:gap-y-4">
+                  {articles.map((a, index) => (
+                    <ArticleItemCard
+                      key={a.id}
+                      article={a}
+                      index={index}
+                      onItemClick={onItemClick}
+                    />
+                  ))}
+                </div>
+              ) : (
+                <div className="w-full">
+                  {articles.map((a, index) => (
+                    <ArticleHorizontalItemCard
+                      key={a.id}
+                      article={a as ArticleModel}
+                      index={index}
+                      onItemClick={onItemClick}
+                      displayText={true}
+                    />
+                  ))}
+                </div>
+              )}
+
+              <div className="flex flex-col text-xs md:flex-row md:justify-between">
+                <ResultsPerPage defaultItemsPerPage={defaultItemsPerPage} />
+                <SearchPagination currentPage={page} totalPages={totalPages} />
+              </div>
+            </section>
+          </React.Fragment>
+        )}
+        {totalItems <= 0 && !isFetching && (
+          // <div className="w-full flex justify-center">
+          //   <h3>0 Results</h3>
+          // </div>
+          <HomeHighlighted rfkId={HIGHLIGHTED_ARTICLES_RFKID} />
+        )}
+      </div>
+    </div>
+  );
+};
+const SearchResultsWidget = widget(
+  SearchResultsComponent,
+  WidgetDataType.SEARCH_RESULTS,
+  'content'
+);
+export default SearchResultsWidget;
