010290086d9e0cc845573daf599ba75f0dec776f
jzi <jzi@sitecore.net>
2025-12-16 19:08:31 -0500

    Reverted some change in how search works

--- industry-verticals/gridwell/src/Bootstrap.tsx
+++ industry-verticals/gridwell/src/Bootstrap.tsx
@@ -2,8 +2,7 @@ import { useEffect, JSX } from 'react';
 import { CloudSDK } from '@sitecore-cloudsdk/core/browser';
 import { SitecorePageProps } from '@sitecore-content-sdk/nextjs';
 import '@sitecore-cloudsdk/events/browser';
-import "@sitecore-cloudsdk/search/browser";
-import scConfig from 'sitecore.config';
+import config from 'sitecore.config';
 
 /**
  * The Bootstrap component is the entry point for performing any initialization logic
@@ -13,35 +12,33 @@ const Bootstrap = (props: SitecorePageProps): JSX.Element | null => {
   const { page } = props;
 
   // Browser ClientSDK init allows for page view events to be tracked
+
   useEffect(() => {
     if (!page) {
       return;
     }
-    //const mode = page.mode;
-    // if (process.env.NODE_ENV === 'development'){
-    //   console.debug('Browser Events SDK is not initialized in development environment');
-    // console.log("oh no, we are in development");}
-    // else if (!mode.isNormal) {
-    //   console.debug('Browser Events SDK is not initialized in edit and preview modes');
-    //   console.log("oh no, we are not in normal pagemode");
-    // }
-    //else {
-    if (scConfig.api.edge?.clientContextId) {
-      CloudSDK({
-        sitecoreEdgeUrl: scConfig.api.edge.edgeUrl,
-        sitecoreEdgeContextId: scConfig.api.edge.clientContextId,
-        siteName: page.siteName || scConfig.defaultSite,
-        enableBrowserCookie: true,
-        // Replace with the top level cookie domain of the website that is being integrated e.g ".example.com" and not "www.example.com"
-        cookieDomain: window.location.hostname.replace(/^www\./, ''),
-      })
-        .addEvents()
-        .addSearch()
-        .initialize()
-    } else {
-      console.error('Client Edge API settings missing from configuration');
+
+    const mode = page.mode;
+    if (process.env.NODE_ENV === 'development')
+      console.debug('Browser Events SDK is not initialized in development environment');
+    else if (!mode.isNormal)
+      console.debug('Browser Events SDK is not initialized in edit and preview modes');
+    else {
+      if (config.api.edge?.clientContextId) {
+        CloudSDK({
+          sitecoreEdgeUrl: config.api.edge.edgeUrl,
+          sitecoreEdgeContextId: config.api.edge.clientContextId,
+          siteName: page.siteName || config.defaultSite,
+          enableBrowserCookie: true,
+          // Replace with the top level cookie domain of the website that is being integrated e.g ".example.com" and not "www.example.com"
+          cookieDomain: window.location.hostname.replace(/^www\./, ''),
+        })
+          .addEvents()
+          .initialize();
+      } else {
+        console.error('Client Edge API settings missing from configuration');
+      }
     }
-    // }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page?.siteName]);
 
